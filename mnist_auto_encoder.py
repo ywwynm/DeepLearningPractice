6633499e437cf1a379532aec1e50aa44a1a63862
ywwynm <ywwynm@gmail.com>
2018-03-22 14:51:45 +0800

    a better version using sigmoid and AdamOptimizer

--- mnist_auto_encoder.py
+++ mnist_auto_encoder.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
@@ -17,40 +19,43 @@ hidden_4_size = 10
 
 weights = {
   'encoder_1': tf.Variable(tf.random_normal([input_size, hidden_1_size])),
-  'decoder_1': tf.Variable(tf.random_normal([hidden_1_size, input_size])),
   'encoder_2': tf.Variable(tf.random_normal([hidden_1_size, hidden_2_size])),
-  'decoder_2': tf.Variable(tf.random_normal([hidden_2_size, hidden_1_size])),
   'encoder_3': tf.Variable(tf.random_normal([hidden_2_size, hidden_3_size])),
-  'decoder_3': tf.Variable(tf.random_normal([hidden_3_size, hidden_2_size])),
   'encoder_4': tf.Variable(tf.random_normal([hidden_3_size, hidden_4_size])),
-  'decoder_4': tf.Variable(tf.random_normal([hidden_4_size, hidden_3_size]))
+
+  'decoder_4': tf.Variable(tf.random_normal([hidden_4_size, hidden_3_size])),
+  'decoder_3': tf.Variable(tf.random_normal([hidden_3_size, hidden_2_size])),
+  'decoder_2': tf.Variable(tf.random_normal([hidden_2_size, hidden_1_size])),
+  'decoder_1': tf.Variable(tf.random_normal([hidden_1_size, input_size]))
 }
 
 biases = {
   'encoder_1': tf.Variable(tf.random_normal([hidden_1_size])),
-  'decoder_1': tf.Variable(tf.random_normal([input_size])),
   'encoder_2': tf.Variable(tf.random_normal([hidden_2_size])),
-  'decoder_2': tf.Variable(tf.random_normal([hidden_1_size])),
   'encoder_3': tf.Variable(tf.random_normal([hidden_3_size])),
-  'decoder_3': tf.Variable(tf.random_normal([hidden_2_size])),
   'encoder_4': tf.Variable(tf.random_normal([hidden_4_size])),
-  'decoder_4': tf.Variable(tf.random_normal([hidden_3_size]))
+
+  'decoder_4': tf.Variable(tf.random_normal([hidden_3_size])),
+  'decoder_3': tf.Variable(tf.random_normal([hidden_2_size])),
+  'decoder_2': tf.Variable(tf.random_normal([hidden_1_size])),
+  'decoder_1': tf.Variable(tf.random_normal([input_size]))
+
 }
 
 
 def encoder(input):
-  l1 = tf.nn.relu(tf.add(tf.matmul(input, weights['encoder_1']), biases['encoder_1']))
-  l2 = tf.nn.relu(tf.add(tf.matmul(l1, weights['encoder_2']), biases['encoder_2']))
-  l3 = tf.nn.relu(tf.add(tf.matmul(l2, weights['encoder_3']), biases['encoder_3']))
-  l4 = tf.nn.relu(tf.add(tf.matmul(l3, weights['encoder_4']), biases['encoder_4']))
+  l1 = tf.nn.sigmoid(tf.add(tf.matmul(input, weights['encoder_1']), biases['encoder_1']))
+  l2 = tf.nn.sigmoid(tf.add(tf.matmul(l1, weights['encoder_2']), biases['encoder_2']))
+  l3 = tf.nn.sigmoid(tf.add(tf.matmul(l2, weights['encoder_3']), biases['encoder_3']))
+  l4 = tf.nn.sigmoid(tf.add(tf.matmul(l3, weights['encoder_4']), biases['encoder_4']))
   return l4
 
 
 def decoder(hidden_layer):
-  l4 = tf.nn.relu(tf.add(tf.matmul(hidden_layer, weights['decoder_4']), biases['decoder_4']))
-  l3 = tf.nn.relu(tf.add(tf.matmul(l4, weights['decoder_3']), biases['decoder_3']))
-  l2 = tf.nn.relu(tf.add(tf.matmul(l3, weights['decoder_2']), biases['decoder_2']))
-  output = tf.nn.relu(tf.add(tf.matmul(l2, weights['decoder_1']), biases['decoder_1']))
+  l4 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_layer, weights['decoder_4']), biases['decoder_4']))
+  l3 = tf.nn.sigmoid(tf.add(tf.matmul(l4, weights['decoder_3']), biases['decoder_3']))
+  l2 = tf.nn.sigmoid(tf.add(tf.matmul(l3, weights['decoder_2']), biases['decoder_2']))
+  output = tf.nn.sigmoid(tf.add(tf.matmul(l2, weights['decoder_1']), biases['decoder_1']))
   return output
 
 
@@ -61,7 +66,7 @@ y_true = X
 y_pred = output
 
 loss = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
@@ -72,3 +77,11 @@ with tf.Session() as sess:
       _, l = sess.run([optimizer, loss], feed_dict={X: batch_x})
       if i % 10 == 0:
         print('epoch: ', epoch, ", i: ", i, ", loss: ", l)
+  validate = sess.run(y_pred, feed_dict={X: mnist.test.images})
+  # print(validate)
+
+  f, a = plt.subplots(2, 10, figsize=(10, 2))
+  for i in range(10):
+    a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
+    a[1][i].imshow(np.reshape(validate[i], (28, 28)))
+  plt.show()
