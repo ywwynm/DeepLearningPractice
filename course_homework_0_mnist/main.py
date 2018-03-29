317ed8f75dadd0ec752d89b51290db173c112c64
ywwynm <ywwynm@gmail.com>
2018-03-29 14:50:17 +0800

    call auto_encoder to get encoded data

--- course_homework_0_mnist/main.py
+++ course_homework_0_mnist/main.py
@@ -2,5 +2,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
+import course_homework_0_mnist.auto_encoder as ae
+
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
+
+encoded_data = ae.get_encoded_data(mnist, mnist.test.images)
