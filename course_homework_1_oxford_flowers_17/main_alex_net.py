354d1d99bececcfc600afd0e6cfb5a0a93148baa
ywwynm <ywwynm@gmail.com>
2018-04-19 00:06:24 +0800

    correct epoch, batch and iterate...

--- course_homework_1_oxford_flowers_17/main_alex_net.py
+++ course_homework_1_oxford_flowers_17/main_alex_net.py
@@ -3,6 +3,8 @@ import course_homework_1_oxford_flowers_17.dataset as dataset
 import matplotlib.pyplot as plt
 import course_homework_1_oxford_flowers_17.alex_net as an
 
+import time
+
 learning_rate = 0.001
 input_width = input_height = 224
 channel = 3
@@ -14,7 +16,6 @@ train_set_1 = dataset.get_train_set(1)
 test_set_1 = dataset.get_test_set(1)
 
 train_set_1 = train_set_1.shuffle(buffer_size=10000)
-train_set_1 = train_set_1.repeat(epochs)
 train_set_1 = train_set_1.batch(batch_size)
 
 X = tf.placeholder(tf.float32, [None, input_height, input_width, channel])
@@ -24,21 +25,27 @@ y_true = tf.placeholder(tf.float32, [None, output_size])
 loss = -tf.reduce_sum(y_true * tf.nn.log_softmax(y_pred)) / batch_size
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
-iterator = train_set_1.make_one_shot_iterator()
+iterator = train_set_1.make_initializable_iterator()
 next_element = iterator.get_next()
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
-  for i in range(epochs):
-    X_batch, Y_batch = sess.run(next_element)
-    # print(X_batch[0][0][0][0])
-    _, l = sess.run([optimizer, loss], feed_dict={X: X_batch, y_true: Y_batch})
-    if i % 10 == 0:
-      # pred = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
-      # accuracy = tf.reduce_mean(tf.cast(pred, "float"))
-      # accuracy_val = sess.run(accuracy, feed_dict={X: test_set_1[0], y_true: test_set_1[1]})
-      print("i: ", i, ", loss: ", l)
-
+  start_train_time = time.time()
+  last_train_op_time = start_train_time
+  for epoch in range(epochs):
+    sess.run(iterator.initializer)
+    batch_idx = 1
+    while True:
+      try:
+        X_batch, Y_batch = sess.run(next_element)
+        # print(X_batch[0][0][0][0])
+        _, l = sess.run([optimizer, loss], feed_dict={X: X_batch, y_true: Y_batch})
+        print("epoch: %d, batch: %d, loss: %f, time cost: %f" % (epoch + 1, batch_idx, l, time.time() - last_train_op_time))
+        batch_idx += 1
+        last_train_op_time = time.time()
+      except tf.errors.OutOfRangeError:  # this epoch ends
+        break
+  print("Training end, total time: %f" % (time.time() - start_train_time))
 
 # with tf.Session() as sess:
 #   sess.run(tf.global_variables_initializer())
