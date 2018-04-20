f2326dd659922c1cf5e3db67fd9de2ea28738d35
ywwynm <ywwynm@gmail.com>
2018-04-20 11:45:27 +0800

    add an VGG implementation(nearly give up AlexNet...)

--- course_homework_1_oxford_flowers_17/vgg_16.py
+++ course_homework_1_oxford_flowers_17/vgg_16.py
@@ -1,38 +1,75 @@
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
+import tensorflow as tf
+import numpy as np
+import math
 
+def conv_2d(input, filter_size, in_channel, out_channel, strides=1):
+  # filter = tf.Variable(tf.truncated_normal([filter_size, filter_size, in_channel, out_channel]) * 1e-4)
 
-def vgg_16():
-  model = Sequential()
+  # input_size = 1.0
+  # for dim in shape[:-1]:
+  #   input_size *= float(dim)
+  # max_val = math.sqrt(3 / (filter_size * filter_size * in_channel)) * 1.0
+  # filter = tf.Variable(tf.random_uniform([filter_size, filter_size, in_channel, out_channel], -max_val, max_val))
 
-  model.add(Conv2D(input_shape=(224, 224, 3), filters=64, kernel_size=3, activation='relu'))
-  model.add(Conv2D(filters=64, kernel_size=3, activation='relu'))
-  model.add(MaxPool2D(strides=2))
+  filter = tf.Variable(tf.truncated_normal([filter_size, filter_size, in_channel, out_channel], stddev=0.1))
 
-  model.add(Conv2D(filters=128, kernel_size=3, activation='relu'))
-  model.add(Conv2D(filters=128, kernel_size=3, activation='relu'))
-  model.add(MaxPool2D(strides=2))
+  conv = tf.nn.conv2d(input, filter, [1, strides, strides, 1], 'SAME')
+  b = tf.Variable(tf.zeros([out_channel]))
+  added = tf.nn.bias_add(conv, b)
+  return tf.nn.relu(added)
 
-  model.add(Conv2D(filters=256, kernel_size=3, activation='relu'))
-  model.add(Conv2D(filters=256, kernel_size=3, activation='relu'))
-  model.add(Conv2D(filters=256, kernel_size=3, activation='relu'))
-  model.add(MaxPool2D(strides=2))
+def max_pool(input, ksize, strides):
+  return tf.nn.max_pool(input, [1, ksize, ksize, 1], [1, strides, strides, 1], 'VALID')
 
-  model.add(Conv2D(filters=512, kernel_size=3, activation='relu'))
-  model.add(Conv2D(filters=512, kernel_size=3, activation='relu'))
-  model.add(Conv2D(filters=512, kernel_size=3, activation='relu'))
-  model.add(MaxPool2D(strides=2))
+def lrn(input, depth_radius=2, bias=2.0, alpha=0.0001, beta=0.75):
+  return tf.nn.lrn(input, depth_radius, bias, alpha, beta)
 
-  model.add(Conv2D(filters=512, kernel_size=3, activation='relu'))
-  model.add(Conv2D(filters=512, kernel_size=3, activation='relu'))
-  model.add(Conv2D(filters=512, kernel_size=3, activation='relu'))
-  model.add(MaxPool2D(strides=2))
+def fully_connected(input, n_units, activation='relu', keep_prob=0.5):
+  input_size = np.prod(input.get_shape().as_list()[1:])
+  flattened = tf.reshape(input, [-1, input_size])
+  # W = tf.Variable(tf.truncated_normal([input_size, n_units]) * 0.001)
+  W = tf.Variable(tf.truncated_normal([input_size, n_units], stddev=0.2))
+  b = tf.Variable(tf.zeros([n_units]))
+  added = tf.nn.xw_plus_b(flattened, W, b)
+  if activation == 'relu':
+    activated = tf.nn.relu(added)
+  elif activation == 'tanh':
+    activated = tf.nn.tanh(added)
+  elif activation == 'softmax':
+    activated = tf.nn.softmax(added)
+  else:
+    activated = added
+  if keep_prob > 0:
+    return tf.nn.dropout(activated, keep_prob=keep_prob)
+  else:
+    return activated
 
-  model.add(Flatten())
-  model.add(Dense(units=4096, activation='relu'))
-  model.add(Dropout(0.5))
-  model.add(Dense(units=4096, activation='relu'))
-  model.add(Dropout(0.5))
-  model.add(Dense(units=17, activation='softmax'))
+def vgg_16(input):
+  net = conv_2d(input, filter_size=3, in_channel=3, out_channel=64, strides=1)
+  net = conv_2d(input, filter_size=3, in_channel=64, out_channel=64, strides=1)
+  net = max_pool(net, ksize=2, strides=2)
 
-  return model
+  net = conv_2d(input, filter_size=3, in_channel=64, out_channel=128, strides=1)
+  net = conv_2d(input, filter_size=3, in_channel=128, out_channel=128, strides=1)
+  net = max_pool(net, ksize=2, strides=2)
+
+  net = conv_2d(input, filter_size=3, in_channel=128, out_channel=256, strides=1)
+  net = conv_2d(input, filter_size=3, in_channel=256, out_channel=256, strides=1)
+  net = conv_2d(input, filter_size=3, in_channel=256, out_channel=256, strides=1)
+  net = max_pool(net, ksize=2, strides=2)
+
+  net = conv_2d(input, filter_size=3, in_channel=256, out_channel=512, strides=1)
+  net = conv_2d(input, filter_size=3, in_channel=512, out_channel=512, strides=1)
+  net = conv_2d(input, filter_size=3, in_channel=512, out_channel=512, strides=1)
+  net = max_pool(net, ksize=2, strides=2)
+
+  net = conv_2d(input, filter_size=3, in_channel=512, out_channel=512, strides=1)
+  net = conv_2d(input, filter_size=3, in_channel=512, out_channel=512, strides=1)
+  net = conv_2d(input, filter_size=3, in_channel=512, out_channel=512, strides=1)
+  net = max_pool(net, ksize=2, strides=2)
+
+  net = fully_connected(net, n_units=4096, activation='relu')
+  net = fully_connected(net, n_units=4096, activation='relu')
+  net = fully_connected(net, n_units=17, activation="", keep_prob=-1.0)
+
+  return net
