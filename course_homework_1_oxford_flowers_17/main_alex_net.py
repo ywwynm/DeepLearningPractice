64b036946bff46f21880ccc8425eff46e8833516
ywwynm <ywwynm@gmail.com>
2018-04-25 21:58:54 +0800

    create a result directory and save final plots to there instead of showing it

--- course_homework_1_oxford_flowers_17/main_alex_net.py
+++ course_homework_1_oxford_flowers_17/main_alex_net.py
@@ -14,7 +14,7 @@ input_width = input_height = 224
 channel = 3
 output_size = 17
 batch_size = 64
-epochs = 200
+epochs = 1
 
 train_set_1 = dataset.get_train_set(1)
 train_set_1 = train_set_1.shuffle(buffer_size=10000)
@@ -83,43 +83,19 @@ with tf.Session() as sess:
 
   print("Classifier has been trained, total time: %f" % (time.time() - start_train_time))
 
-  # pred_vals = []
-  # while True:
-  #   try:
-  #     X_batch_tst, Y_batch_tst = sess.run(next_element_tst_1)
-  #     pred = tf.cast(tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1)), tf.float32)
-  #     pred_val = sess.run(pred, feed_dict={X: X_batch_tst, y_true: Y_batch_tst})
-  #     pred_vals.append(pred_val)
-  #   except tf.errors.OutOfRangeError:
-  #     break
-  # print("accuracy: %f" % (np.mean(pred_vals)))
+  import os
+  if not os.path.exists("result"):
+    os.mkdir("result")
 
-# with tf.Session() as sess:
-#   sess.run(tf.global_variables_initializer())
-#   times = []
-#   cross_entropy_losses = []
-#   error_rates = []
-#
-#   print("Start to train classifier --------------------")
-#   for i in range(epoches):
-#     batch_x, batch_y = mnist.train.next_batch(batch_size)
-#     _, l = sess.run([optimizer, loss], feed_dict={X: batch_x, y_true: batch_y})
-#     if i % 10 == 0:
-#       times.append(i)
-#       cross_entropy_losses.append(l)
-#       pred = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
-#       accuracy = tf.reduce_mean(tf.cast(pred, "float"))
-#       accuracy_val = sess.run(accuracy, feed_dict={X: test_set_1[0], y_true: test_set_1[1]})
-#       error_rates.append(1 - accuracy_val)
-#       print("i: ", i, ", loss: ", l, ", error rate: ", 1 - accuracy_val)
-#   print("Classifier has been trained --------------------")
-#
-#   plt.plot(times, cross_entropy_losses)
-#   plt.xlabel('trained times')
-#   plt.ylabel('cross entropy')
-#   plt.show()
-#
-#   plt.plot(times, error_rates)
-#   plt.xlabel('trained times')
-#   plt.ylabel('error rate')
-#   plt.show()
+  post_fix = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+
+  plt.plot(epochs_arr, losses)
+  plt.xlabel('epoch')
+  plt.ylabel('loss')
+  plt.savefig("result/alex_net_loss_" + post_fix + ".png")
+
+  plt.plot(epochs_10_arr, accuracies)
+  plt.xlabel('epoch')
+  plt.ylabel('accuracy')
+  plt.savefig("result/alex_net_acc_" + post_fix + ".png")
+
