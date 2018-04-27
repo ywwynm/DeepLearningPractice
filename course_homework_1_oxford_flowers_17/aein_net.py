d6881a63709c7224a69082edcc1135e8b0154b23
ywwynm <ywwynm@gmail.com>
2018-04-27 15:32:13 +0800

    extract duplicated code blocks into reusable methods

--- course_homework_1_oxford_flowers_17/aein_net.py
+++ course_homework_1_oxford_flowers_17/aein_net.py
@@ -1,57 +1,6 @@
 import tensorflow as tf
 import numpy as np
-import math
-
-def conv_2d(id, input, filter_size, in_channel, out_channel, strides=1, padding='SAME'):
-  # filter = tf.Variable(tf.truncated_normal([filter_size, filter_size, in_channel, out_channel]) * 1e-4)
-
-  # input_size = 1.0
-  # for dim in shape[:-1]:
-  #   input_size *= float(dim)
-  # max_val = math.sqrt(3 / (filter_size * filter_size * in_channel)) * 1.0
-  # filter = tf.Variable(tf.random_uniform([filter_size, filter_size, in_channel, out_channel], -max_val, max_val))
-
-  # filter = tf.Variable(tf.truncated_normal([filter_size, filter_size, in_channel, out_channel], stddev=0.1))
-
-  filter = tf.get_variable("W_" + str(id), [filter_size, filter_size, in_channel, out_channel],
-                           initializer=tf.contrib.layers.xavier_initializer())
-
-  conv = tf.nn.conv2d(input, filter, [1, strides, strides, 1], padding)
-  # b = tf.Variable(tf.zeros([out_channel]))
-  b = tf.get_variable("b_" + str(id), [out_channel],
-                  initializer=tf.contrib.layers.xavier_initializer())
-  added = tf.nn.bias_add(conv, b)
-  return tf.nn.relu(added)
-
-def max_pool(input, ksize, strides, padding='SAME'):
-  return tf.nn.max_pool(input, [1, ksize, ksize, 1], [1, strides, strides, 1], padding)
-
-def lrn(input, depth_radius=5, bias=1.0, alpha=1e-4, beta=0.75):
-  return tf.nn.lrn(input, depth_radius, bias, alpha, beta)
-
-def fully_connected(id, input, n_units, activation='relu', keep_prob=0.5):
-  input_size = np.prod(input.get_shape().as_list()[1:])
-  flattened = tf.reshape(input, [-1, input_size])
-  # W = tf.Variable(tf.truncated_normal([input_size, n_units]) * 0.001)
-  # W = tf.Variable(tf.truncated_normal([input_size, n_units], stddev=0.2))
-  W = tf.get_variable("W_" + str(id), [input_size, n_units],
-                           initializer=tf.contrib.layers.xavier_initializer())
-  # b = tf.Variable(tf.zeros([n_units]))
-  b = tf.get_variable("b_" + str(id), [n_units],
-                  initializer=tf.contrib.layers.xavier_initializer())
-  added = tf.nn.xw_plus_b(flattened, W, b)
-  if activation == 'relu':
-    activated = tf.nn.relu(added)
-  elif activation == 'tanh':
-    activated = tf.nn.tanh(added)
-  elif activation == 'softmax':
-    activated = tf.nn.softmax(added)
-  else:
-    activated = added
-  if keep_prob > 0:
-    return tf.nn.dropout(activated, keep_prob=keep_prob)
-  else:
-    return activated
+from net import conv_2d, max_pool ,lrn, fully_connected
 
 
 def aein_net(input):
---
+++ course_homework_1_oxford_flowers_17/model.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+import dataset
+import aein_net, alex_net, vgg_16
+import utils
+import time
+
+def train_and_evaluate(net_name="aein_net", epochs = 250, train_batch_size=64, learning_rate=1e-3, optimizer="adam"):
+  train_set_size = 680
+  val_test_set_size = 340
+
+  input_width = input_height = 224
+  channel = 3
+  output_size = 17
+
+  train_set_1 = dataset.get_train_set(1)
+  train_set_1 = train_set_1.shuffle(buffer_size=10000)
+  train_set_1 = train_set_1.batch(train_batch_size)
+
+  validation_set_1 = dataset.get_validation_set(1)
+  validation_set_1 = validation_set_1.batch(64)
+
+  test_set_1 = dataset.get_test_set(1)
+  test_set_1 = test_set_1.batch(64)
+
+  X = tf.placeholder(tf.float32, [None, input_height, input_width, channel])
+  if net_name == 'aein_net':
+    y_pred = aein_net.aein_net(X)
+  elif net_name == 'alex_net':
+    y_pred = alex_net.alex_net(X)
+  elif net_name == 'vgg_16':
+    y_pred = vgg_16.vgg_16(X)
+  else:
+    y_pred = aein_net.aein_net(X)
+  y_true = tf.placeholder(tf.float32, [None, output_size])
+
+  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_pred, labels=y_true))
+
+  if optimizer == 'adam':
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+  elif optimizer == 'momentum':
+    optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9)
+  elif optimizer == 'gradient_descent':
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+  elif optimizer == 'rmsprop':
+    optimizer = tf.train.RMSPropOptimizer(learning_rate)
+  else:
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+  optimizer = optimizer.minimize(loss)
+
+  itr_trn_1 = train_set_1.make_initializable_iterator()
+  next_element_trn_1 = itr_trn_1.get_next()
+
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    start_train_time = time.time()
+    accuracies = []
+    losses = []
+    epochs_arr = []
+    epochs_10_arr = []
+    print("Start to train classifier, model: " + net_name)
+    for epoch in range(epochs):
+      print("------------------ Epoch %d starts ------------------" % (epoch + 1))
+      sess.run(itr_trn_1.initializer)
+      batch_idx = 1
+      while True:
+        try:
+          X_batch, Y_batch = sess.run(next_element_trn_1)
+          last_train_op_time = time.time()
+          _, l = sess.run([optimizer, loss], feed_dict={X: X_batch, y_true: Y_batch})
+          losses.append(l)
+          epochs_arr.append(epoch)
+          print("epoch: %d, batch: %d, loss: %f, time cost: %f" % (
+          epoch + 1, batch_idx, l, time.time() - last_train_op_time))
+          batch_idx += 1
+        except tf.errors.OutOfRangeError:  # this epoch ends
+          break
+
+      if (epoch + 1) % 10 == 0:
+        print()
+        print("------------------ evaluating accuracy on validation set ------------------")
+        accuracy = utils.evaluate(validation_set_1, sess, X, y_true, y_pred)
+        print("accuracy: %f" % accuracy)
+        print()
+        accuracies.append(accuracy)
+        epochs_10_arr.append(epoch + 1)
+
+    print("Classifier has been trained, total time: %f" % (time.time() - start_train_time))
+    print()
+    print("------------------ evaluating accuracy on test set ------------------")
+    print("accuracy: %f" % utils.evaluate(test_set_1, sess, X, y_true, y_pred))
+    print()
+
+    utils.save_result(net_name, epochs_arr, losses, epochs_10_arr, accuracies)
