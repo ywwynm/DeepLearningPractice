7bae0c46f6d57c307f0500f304826154d147b56b
ywwynm <ywwynm@gmail.com>
2018-06-09 21:27:22 +0800

    start to load imdb database

---
+++ course_homework_2_stanford_sentiment_treebank/STANFORD_SENTIMENT_TREEBANK_data/dataset_imdb.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+def get_imdb_dataset_maybe_download():
+  dataset_path = tf.keras.utils.get_file(
+    fname="aclImdb.tar.gz",
+    origin="http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz",
+    extract=True)
